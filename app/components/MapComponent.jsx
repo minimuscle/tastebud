801688f84c0748f9b817f7ed7eb19e666613df9c
minimuscle <joshthiele@live.com.au>
2023-03-06 21:08:28 +1100

    added mapbox

---
+++ app/components/MapComponent.jsx
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect, useState } from 'react'
+import mapboxgl from 'mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+
+export default function MapComponent(props) {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+  mapboxgl.accessToken = props.API
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom,
+    })
+  })
+
+  return (
+    <div>
+      <div ref={mapContainer} className='map-container' />
+    </div>
+  )
+}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,20 @@
+import { useLoaderData } from '@remix-run/react'
+import MapComponent from '~/components/MapComponent'
+
 export default function Index() {
+  const API = useLoaderData()
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <MapComponent API={API} />
     </div>
-  );
+  )
+}
+
+export function loader() {
+  const API = process.env.MAPS_ACCESS_TOKEN
+  return API || null
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: '../styles/index.css' }]
 }
