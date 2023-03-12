6f407816fc787bc6a608840931e96336e582746d
minimuscle <joshthiele@live.com.au>
2023-03-13 10:47:15 +1100

    geo location

--- app/components/MapComponent.jsx
+++ app/components/MapComponent.jsx
@@ -1,14 +1,14 @@
-import React, { useRef, useEffect, useState } from "react"
-import mapboxgl from "mapbox-gl" // eslint-disable-line import/no-webpack-loader-syntax
-import Sidebar from "~/components/Sidebar"
-import { Center, Button } from "@chakra-ui/react"
+import React, { useRef, useEffect, useState } from 'react'
+import mapboxgl from 'mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+import Sidebar from '~/components/Sidebar'
+import { Center, Button } from '@chakra-ui/react'
 
 export default function MapComponent(props) {
   const mapContainer = useRef(null)
   const map = useRef(null)
-  const [lng, setLng] = useState(144.96)
+  const [lng, setLng] = useState(140.96)
   const [lat, setLat] = useState(-37.82)
-  const [food, setFood] = useState("")
+  const [food, setFood] = useState('')
   const [zoom, setZoom] = useState(12)
   const [marker, addMarker] = useState([])
   mapboxgl.accessToken = props.API
@@ -17,30 +17,41 @@ export default function MapComponent(props) {
     if (map.current) return // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
+      style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
       zoom: zoom,
     })
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+    })
+    map.current.addControl(geolocate)
+    map.current.on('load', () => {
+      geolocate.trigger()
+    })
 
-    document.addEventListener("DOMContentLoaded", () => map.resize())
+    document.addEventListener('DOMContentLoaded', () => map.resize())
   })
 
   //TODO: This needs to update the sidebar
   useEffect(() => {
     if (!map.current) return // wait for map to initialize
-    map.current.on("move", () => {
+    map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4))
       setLat(map.current.getCenter().lat.toFixed(4))
       setZoom(map.current.getZoom().toFixed(2))
     })
-    map.current.on("contextmenu", () => {
-      console.log("CLICKED")
+    map.current.on('contextmenu', () => {
+      console.log('CLICKED')
     })
     console.log(lat)
   })
 
   const search = async () => {
-    console.log("searching")
+    console.log('searching')
     const data = await fetch(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/restaurant.json?proximity=${lng}%2C${lat}&limit=10&access_token=${props.API}`
     )
@@ -59,7 +70,7 @@ export default function MapComponent(props) {
           .setLngLat([location.center[0], location.center[1]])
           .addTo(map.current)
       )
-      console.log(locations)
+      return console.log(locations)
     })
     addMarker(marker.concat(locations))
     console.log(marker)
@@ -79,7 +90,7 @@ export default function MapComponent(props) {
           hidden={!food}
           className='clickable'
         >
-          Search Area For {food.slice(-1) === "s" ? food : `${food}s`}
+          Search Area For {food.slice(-1) === 's' ? food : `${food}s`}
         </Button>
       </Center>
     </div>
