6b8b83e6f8838fbde20a9188ad5f6e310ddfc2fc
minimuscle <joshthiele@live.com.au>
2023-03-23 16:21:04 +1100

    fixed charka ui

--- app/components/LocationPop.jsx
+++ app/components/LocationPop.jsx
@@ -12,16 +12,19 @@ import {
   Box,
   Text,
   Heading,
+  ChakraProvider,
 } from "@chakra-ui/react"
 
 export default function LocationPop(props) {
   const location = props.location
   return (
-    <Box>
-      <Heading as='h2' size='md'>
-        {location.name}
-      </Heading>
-      <Text>{location.address}</Text>
-    </Box>
+    <ChakraProvider theme={AppTheme}>
+      <Box>
+        <Heading as='h2' size='md'>
+          {location.name}
+        </Heading>
+        <Text>{location.address}</Text>
+      </Box>
+    </ChakraProvider>
   )
 }
