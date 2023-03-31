e7be05e50e5f8ef80f99a7b9eb6082af1109038f
minimuscle <joshthiele@live.com.au>
2023-03-31 21:19:25 +1100

    basic signup connection to google

--- app/routes/_auth.login.jsx
+++ app/routes/_auth.login.jsx
@@ -1,3 +1,21 @@
+import { redirect } from '@remix-run/node'
+import { createClient } from '@supabase/supabase-js'
+
 export default function Login() {
-  return <div>Login Form</div>
+  return (
+    <div>
+      Login Form
+      <form method="POST">
+        <button type="submit">Get Session</button>
+      </form>
+    </div>
+  )
+}
+
+export async function action({ request }) {
+  const supabase = createClient(process.env.DATABASE, process.env.SUPABASE_KEY)
+  const { data, error } = await supabase.auth.getSession()
+  console.log(error)
+  console.log(data)
+  return redirect('/')
 }
--- app/routes/_auth.signup.jsx
+++ app/routes/_auth.signup.jsx
@@ -1,3 +1,22 @@
+import { redirect } from '@remix-run/node'
+import { createClient } from '@supabase/supabase-js'
+
 export default function SignUp() {
-  return <div>_auth.signup</div>
+  return (
+    <div>
+      <form method="POST">
+        <button type="submit">Sign in with Google</button>
+      </form>
+    </div>
+  )
+}
+
+export async function action({ request }) {
+  const supabase = createClient(process.env.DATABASE, process.env.SUPABASE_KEY)
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+  })
+  console.log(error)
+  console.log(data)
+  return redirect(data.url)
 }
