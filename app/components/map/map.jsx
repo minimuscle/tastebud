dc8b7f4f4dca2f24b25a89d120031576902e9861
minimuscle <joshthiele@live.com.au>
2023-06-13 00:17:46 +1000

    added mapcomponent

--- app/components/map/map.jsx
+++ app/components/map/map.jsx
@@ -1,31 +1,36 @@
-import { GoogleMap, MarkerF, InfoBox, InfoWindow } from '@react-google-maps/api'
+import {
+  GoogleMap,
+  MarkerF,
+  InfoBox,
+  InfoWindow,
+  useGoogleMap,
+} from '@react-google-maps/api'
 import { useFetcher, useSearchParams } from '@remix-run/react/dist'
 import { useEffect, useMemo } from 'react'
-import PoiPopup from '~/components/map/poiPopup'
-import * as ReactDOMClient from 'react-dom/client'
+import MapComponents from '~/components/map/mapComponents'
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams()
   const lat = searchParams.get('lat') || -37.8148
   const lng = searchParams.get('lng') || 144.9638
-  const placeFetcher = useFetcher()
-
-  useEffect(() => {
-    console.log(placeFetcher.state)
-  }, [placeFetcher.state])
 
   const mapLoad = (map) => {
-    map.addListener('click', (event) => {
+    /*map.addListener('click', async (event) => {
       if (event.placeId) {
         event.stop()
         //Search for the place ID on supabase before attempting to render the popup
         const placeId = event.placeId
         placeFetcher.submit({ placeId: placeId }, { method: 'post' })
-
         //FIXME: This doesn't actually wait yet. It needs to await the fetcher
-        placeFetcher.state === 'idle' &&
-          placeFetcher.data &&
-          console.log(placeFetcher.data)
+        if (placeFetcher.state === 'idle' && placeFetcher.data) {
+          const infowindow = new window.google.maps.InfoWindow()
+          const popupNode = document.createElement('React.Fragment')
+          const root = ReactDOMClient.createRoot(popupNode)
+          root.render(<PoiPopup />)
+          infowindow.setPosition(lat, lng)
+          infowindow.setContent(popupNode)
+          infowindow.open(map)
+        }
 
         // Create a new React.Fragment element to hold the React component
         // Create a new React root and render the PoiPopup component into it
@@ -37,7 +42,7 @@ export default function Map() {
         infowindow.setContent(popupNode)
         infowindow.open(map)
       }
-    })
+    })*/
   }
 
   const center = useMemo(() => ({ lat: -37.8148, lng: 144.9638 }), [])
@@ -50,10 +55,7 @@ export default function Map() {
         mapLoad(map)
       }}
     >
-      <InfoWindow position={center}>
-        <PoiPopup />
-      </InfoWindow>
-      <MarkerF position={{ lat: -37.8148, lng: 144.9638 }} />
+      <MapComponents />
     </GoogleMap>
   )
 }
